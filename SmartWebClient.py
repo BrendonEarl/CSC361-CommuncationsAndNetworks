3ab8f42ae4f9a159d2c3961c51bd0bf3d89acf3f
Brendon Earl <bearl@uvic.ca>
2018-01-23 12:47:00 -0800

    rename url to uri

--- SmartWebClient.py
+++ SmartWebClient.py
@@ -3,25 +3,27 @@ import socket
 from urllib.parse import urlparse, urlunparse
 
 class SmartWebClient():
-    def  __init__(self, parsedURL = {}):
-        url = urlunparse(parsedURL)
+    def  __init__(self, parsedURI = {}):
+        url = urlunparse(parsedURI)
         sys.stdout.write('Starting Smart Web Client\n\n')
         # open server socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((url, 80))
-        self.findHttpScheme(url)
+        self.sock.connect((uri, 80))
+        self.findHttpScheme(uri)
 
-    def findHttpScheme(self, url):
+    def findHttpScheme(self, uri):
         print("-----Finding available HTTP scheme---")
-        self.httpSend("GET", url, 'HTTP/2')
-        self.httpRecv()
+        self.httpSend("HEAD", uri, 'HTTP/1.1')
+        resp = self.httpRecv()
+    
+    def findHttpProtocol(sef, uri)
 
-    def httpSend(self, method, url, httpV,):
+    def httpSend(self, method, uri, httpV,):
         print("---Request begin---")
 
-        req = "{0} {1} {2}\r\n\r\n".format(method, url, httpV)
+        req = "{0} {1} {2}\r\n\r\n".format(method, uri, httpV)
         print(req.strip())
-        print("Host: {0}".format(url))
+        print("Host: {0}".format(uri))
         print("Connection: Keep-Alive")
         self.sock.send(req.encode())
 
@@ -35,13 +37,14 @@ class SmartWebClient():
         if (len(data) > 1):
             print("\n---Response body---")
             print(data[1])
+        return(data)
 
 
 if __name__ == "__main__":
     try:
         # TODO: Add proper error handling of bad URIs
-        parsedURL = urlparse(sys.argv[1])
-        SmartWebClient(parsedURL)
+        parsedURI = urlparse(sys.argv[1])
+        SmartWebClient(parsedURI)
     except ValueError:
         sys.stderr.write('Incorrect URI format. Try again')
     
