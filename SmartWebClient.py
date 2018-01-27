326b44cc632726611b40233598e4c45861ac9d83
Brendon Earl <bearl@uvic.ca>
2018-01-26 21:42:19 -0800

    Lint project

--- SmartWebClient.py
+++ SmartWebClient.py
@@ -3,11 +3,13 @@ import ssl
 import socket
 from urllib.parse import urlparse
 
+
 class SmartWebClient():
-    def  __init__(self, url):
+    def __init__(self, url):
         self.sock = None
         self.protocol = None
         self.cookies = []
+
         try:
             self.url = urlparse("https://{}".format(url))
         except ValueError:
@@ -18,65 +20,73 @@ class SmartWebClient():
         print('\n.............................')
         print('..Starting Smart Web Client..')
         print('.............................\n')
-        print("---\/ Finding available HTTP scheme and snatching cookies  \/ ---\n")
-        resp = self.findHttpScheme()
-        print("\n---\/ Testing for HTTP/2 availability \/---\n")
+
+        print("---\\/ Finding available HTTP scheme and snatching cookies  \\/ ---\n")
+        self.findHttpScheme()
+
+        print("\n---\\/ Testing for HTTP/2 availability \\/---\n")
         self.testHttp2()
-        print("\n\n---\/ Solution \/---\n")
-        self.reportSoln(resp)
+
+        print("\n\n---\\/ Solution \\/---\n")
+        self.reportSoln()
+
         print()
         self.closeHttpSocket(self.sock)
 
-
-    def reportSoln(self, resp):
+    def reportSoln(self):
         print("website: {}".format(self.url.netloc))
-        print("1. Support of HTTPS: {}".format("yes" if self.url.scheme == 'https' else "no"))
-        print("2. The newest HTTP versions that the server supports: {}".format(self.protocol))
+        print("1. Support of HTTPS: {}".format(
+            "yes" if self.url.scheme == 'https' else "no"))
+        print("2. The newest HTTP versions that the server supports: {}".format(
+            self.protocol))
         print("3. List of Cookies:")
-        if self.cookies != None:
+        if self.cookies is not None:
             for cookie in self.cookies:
-            # for cookie in [dict(cookies) for cookies in set([tuple(cookie.items()) for cookie in self.cookies])]:
                 print("name: -, key: {}{}".format(
                     cookie['key'],
-                    ", domain-name: {}".format(cookie['domain']) if cookie['domain'] != None else ""
+                    ", domain-name: {}".format(cookie['domain']
+                                               if cookie['domain'] is not None else "")
                 ))
-
 
     def findHttpScheme(self):
         self.sock = self.openHttpSocket(self.url)
-
         self.httpSend("HEAD", self.url, 'HTTP/1.1')
-        header, body = self.parseResponse(self.httpRecv())
-        if (header == None): return None
 
-        if(header['status-code'] == 505):
+        header, body = self.parseResponse(self.httpRecv())
+        if header is None:
+            return None
+
+        if header['status-code'] == 505:
             self.protocol = header['http-version']
-        elif(header['status-code'] == 302 or header['status-code'] == 301):
+        elif header['status-code'] == 302 or header['status-code'] == 301:
             self.protocol = header['http-version']
             self.closeHttpSocket(self.sock)
+
             if 'location' in header:
                 self.url = urlparse(header['location'])
-            if (self.url.scheme == ''): self.url.scheme = 'http'
+            if self.url.scheme == '':
+                self.url.scheme = 'http'
+
             self.findHttpScheme()
-        elif(header['status-code'] == 200):
+        elif header['status-code'] == 200:
             self.protocol = 'HTTP/1.1'
         else:
             print("Unexpected server error: {}".format(header['status-code']))
-        
-        return header
 
+        return header
 
     def testHttp2(self):
         self.closeHttpSocket(self.sock)
         self.sock = self.openHttpSocket(self.url, True)
-        if(self.url.scheme == 'https' and \
-            (self.sock.selected_alpn_protocol() == 'h2' or self.sock.selected_npn_protocol() == 'h2')
-            ):
-                self.protocol = 'HTTP/2'
 
+        if (self.url.scheme == 'https' and (
+            self.sock.selected_alpn_protocol() == 'h2' or
+            self.sock.selected_npn_protocol() == 'h2'
+        )):
+            self.protocol = 'HTTP/2'
 
     def httpSend(self, method, parsedURL, httpV):
-        if (self.sock != None):
+        if self.sock is not None:
             print("-Request begin-")
 
             req = "{} {} {}\r\nhost: {}\r\nconnection: {}\r\n\r\n".format(
@@ -95,9 +105,8 @@ class SmartWebClient():
         else:
             print("No Socket Initialized")
 
-
     def httpRecv(self):
-        if (self.sock != None):
+        if self.sock is not None:
             try:
                 resp = self.sock.recv().decode()
             except TypeError:
@@ -106,13 +115,13 @@ class SmartWebClient():
             except ConnectionResetError:
                 print('!! Connection dropped by peer')
                 return None
-                
+
             splitResp = resp.split("\r\n\r\n")
 
             print("\n-Response header-")
             print(splitResp[0])
 
-            if (len(splitResp) > 1):
+            if len(splitResp) > 1:
                 print("\n-Response body-")
                 print(splitResp[1])
 
@@ -120,7 +129,6 @@ class SmartWebClient():
         else:
             print("No Socket Initialized")
             return None
-
 
     def parseResponse(self, resp):
         parsedResponse = {}
@@ -132,7 +140,7 @@ class SmartWebClient():
             return (None, None)
 
         header = splitResp[0]
-        if (len(splitResp) > 1):
+        if len(splitResp) > 1:
             body = splitResp[1]
         else:
             body = None
@@ -148,7 +156,7 @@ class SmartWebClient():
 
         for attribute in splitHeader[1:]:
             splitAttribute = attribute.split(": ")
-            if(splitAttribute[0].lower() != 'set-cookie'):
+            if splitAttribute[0].lower() != 'set-cookie':
                 try:
                     parsedResponse.update({
                         splitAttribute[0].lower(): splitAttribute[1]
@@ -162,11 +170,13 @@ class SmartWebClient():
                 domain = None
 
                 for crumb in crumbs:
-                    if ('domain' in crumb):
+                    if 'domain' in crumb:
                         try:
                             domain = crumb.split('=')[1]
                         except IndexError:
-                            print('!! Malformed header domain cookie: {}'.format(splitAttribute))
+                            print('!! Malformed header domain cookie: {}'.format(
+                                splitAttribute
+                            ))
 
                 cookie = {
                     'key': key,
@@ -174,17 +184,16 @@ class SmartWebClient():
                 }
 
                 self.cookies.append(cookie)
-        return (parsedResponse, body)        
+        return (parsedResponse, body)
 
-
-    def openHttpSocket(self, parsedURL, h2 = False):
+    def openHttpSocket(self, parsedURL, h2=False):
         SCHEME = "HTTPS" if parsedURL.scheme == 'https' else "HTTP"
         PORT = 443 if parsedURL.scheme == 'https' else 80
         PROTOCOLS = ['h2', 'http/1.1'] if h2 else ['http/1.1']
 
         print("-Opening {} Socket on Port {}-".format(SCHEME, PORT))
 
-        if (SCHEME == 'HTTPS'):
+        if SCHEME == 'HTTPS':
             ctx = ssl.create_default_context()
             ctx.set_alpn_protocols(PROTOCOLS)
             ctx.set_npn_protocols(PROTOCOLS)
@@ -204,9 +213,8 @@ class SmartWebClient():
             return self.openHttpSocket(self.url)
         return sock
 
-
     def closeHttpSocket(self, sock):
-        if (sock != None):
+        if sock is not None:
             sock.close()
             print("-Socket Closed-")
         else:
@@ -215,7 +223,7 @@ class SmartWebClient():
 
 if __name__ == "__main__":
     try:
-        if (sys.argv[1] == None):
+        if sys.argv[1] is None:
             print("No URL provided")
         else:
             SmartWebClient(sys.argv[1])
@@ -224,4 +232,3 @@ if __name__ == "__main__":
         e = sys.exc_info()[0]
         print(e)
         # raise e
-    
